fbcba1dccff021ccfd071cd555281b624a69ea58
Dawnfrosty <88573246+Dawnfrosty@users.noreply.github.com>
2023-05-08 19:34:13 +0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -80,3 +80,10 @@ handler.command = /^(menu|help|\?)$/i
 export default handler
 
 const readMore = (String.fromCharCode(8206)).repeat(4001)
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
